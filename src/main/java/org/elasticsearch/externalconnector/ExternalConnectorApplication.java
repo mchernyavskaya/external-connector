2db7f1d4043e65224077d2c31cb844071a7c0859
Maryna Cherniavska <maryna.cherniavska@elastic.co>
2022-01-18 17:35:00 +0100

    initial

---
+++ src/main/java/org/elasticsearch/externalconnector/ExternalConnectorApplication.java
@@ -0,0 +1,13 @@
+package org.elasticsearch.externalconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/elasticsearch/externalconnector/ExternalConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elasticsearch.externalconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
